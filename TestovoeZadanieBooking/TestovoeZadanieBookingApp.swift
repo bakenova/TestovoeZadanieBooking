4ce39cb78cc71fde095962eb058420b47be49c1d
Arailym <arailym.bakenovaa@gmail.com>
2023-09-08 09:52:14 +0600

    Initial Commit

---
+++ TestovoeZadanieBooking/TestovoeZadanieBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestovoeZadanieBookingApp.swift
+//  TestovoeZadanieBooking
+//
+//  Created by Арайлым Бакенова on 08.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestovoeZadanieBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
